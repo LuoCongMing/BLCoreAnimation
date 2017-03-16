5f9b561d990cef70c34d17c0f24d59ca81ac77d0
周建波 <zhoujianbo@zhoujianbodeMacBook-Pro.local>
2017-03-16 16:01:55 +0800

    day 1 ending..

--- BLCoreAnimation/1Day/FirstDayViewController.m
+++ BLCoreAnimation/1Day/FirstDayViewController.m
@@ -10,29 +10,30 @@
 #import "ContentsRectViewController.h"
 
 @interface FirstDayViewController ()
+{
+    
+    CALayer*blueLayer;
+}
 @property (weak, nonatomic) IBOutlet UIView *layerView;
-
 @end
-
 @implementation FirstDayViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
     // image 加到view 上面
-    CALayer*blueLayer = [[CALayer alloc]init];
+    blueLayer = [[CALayer alloc]init];
     blueLayer.frame = CGRectMake(50, 50, 100, 100);
     blueLayer.backgroundColor = [UIColor blueColor].CGColor;
     [_layerView.layer addSublayer:blueLayer];
     
-    UIImage*image = [UIImage imageNamed:@"1day"];
+    UIImage*image = [UIImage imageNamed:@"flowers"];
     blueLayer.contents = (__bridge id)image.CGImage;
     
     // contentsGravity 相当于view 中的 contentModel属性，用来控制显示内容
-    blueLayer.contentsGravity = kCAGravityResizeAspect;
+    blueLayer.contentsGravity = kCAGravityResize;
    
     //contentsScale 这个属性的值用来控制每一个点显示多少个像素、1.0的时候就是一个像素一个点、2.0的时候就2*2个像素一个点（retina屏）,当设置为kCAGravityResizeAspect的时候这个值是无效的
-    blueLayer.contentsScale = image.scale;
+//    blueLayer.contentsScale = image.scale;
     
     //设置了 是否裁剪边界外的内容 对应的view方法叫 clipsToBounds
     
@@ -47,6 +48,12 @@
     ContentsRectViewController*content = [ContentsRectViewController new];
     [self.navigationController pushViewController:content animated:YES];
     
+}
+- (IBAction)contentCenter:(id)sender {
+    
+    //contentsCenter 其实是一个CGRect，它定义了一个固定的边框和一个在图 层上可拉伸的区域。 改变 contentsCenter 的值并不会影响到寄宿图的显示，除 非这个图层的大小改变了，你才看得到效果。  默认是{0, 0, 1, 1}
+    
+    blueLayer.contentsCenter = CGRectMake(0.5, 0, 0.5, 0.5);//设置拉伸范围为左下角
 }
 
 
